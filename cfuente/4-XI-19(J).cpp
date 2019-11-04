2cc3a653394e2b4181798473b45af4753a945075
resueltosCortijo <57351371+resueltosCortijo@users.noreply.github.com>
2019-11-04 15:37:43 +0100

    Update and rename 4-XI-19(Javivi).cpp to 4-XI-19(J).cpp

--- cfuente/4-XI-19(J).cpp
+++ cfuente/4-XI-19(J).cpp
@@ -1,24 +1,24 @@
 /***************************************************************************/
-// DEPARTAMENTO DE CIENCIAS DE LA COMPUTACI”N E INTELIGENCIA ARTIFICIAL
+// DEPARTAMENTO DE CIENCIAS DE LA COMPUTACI√ìN E INTELIGENCIA ARTIFICIAL
 // CURSO 2019-2020
 //
-// FUNDAMENTOS DE PROGRAMACI”N
-// GRADO EN INGENIERÕA INFORM¡TICA
+// FUNDAMENTOS DE PROGRAMACI√ìN
+// GRADO EN INGENIER√çA INFORM√ÅTICA
 // GRUPO C
 //
-// EXAMEN PR¡CTICO 1 - NOVIEMBRE 2019
+// EXAMEN PR√ÅCTICO 1 - NOVIEMBRE 2019
 // 
-// °RELLENAD LOS SIGUIENTES DATOS!
+// ¬°RELLENAD LOS SIGUIENTES DATOS!
 //
-// Apellidos: ExpÛsito MartÌnez
-// Nombre:  Javier
+// Apellidos: ¬ø?
+// Nombre:  ¬ø?
 //
-// GRUPO DE PR¡CTICAS: 1 
+// GRUPO DE PR√ÅCTICAS: 1 
 // EJERCICIO: 1 
 //
 // Fecha: 4-11-2019
 // 
-// Ordenador (ver etiqueta): ei140204 Isla 3 PC 3
+// Ordenador (ver etiqueta): ¬ø?
 //
 /*********************************************************************/
 
@@ -32,7 +32,7 @@ using namespace std;
 
 int main()
 {
-    cout.setf(ios::fixed);		// NotaciÛn de punto fijo para los reales
+    cout.setf(ios::fixed);		// Notaci√≥n de punto fijo para los reales
 	cout.setf(ios::showpoint);	// Mostrar siempre decimales
 
 	int suma=0;
@@ -42,7 +42,7 @@ int main()
 	bool encontrado=false;
 	string continua;
   do{
-     cout<<"Introduzca un ¥n˙mero mayor o igual a 0"<<endl;
+     cout<<"Introduzca un ¬¥n√∫mero mayor o igual a 0"<<endl;
 	  cin>>num;
 	  for(int i=0;i<num && encontrado==false;i++){
 	     for(int j=1;j<num && encontrado==false;j++){
