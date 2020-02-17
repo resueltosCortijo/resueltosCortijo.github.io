bcae9886349e3f8f62220439dded9fc27a754dcf
resueltosCortijo <57351371+resueltosCortijo@users.noreply.github.com>
2020-02-17 10:31:53 +0100

    Update Conversor_señal_analógica_a_número_decimal_C++.cpp

--- electronica/Conversor_señal_analógica_a_número_decimal_C++.cpp
+++ electronica/Conversor_señal_analógica_a_número_decimal_C++.cpp
@@ -7,17 +7,18 @@ using namespace std;
 
 int main(){
 	
-	double va;
+	double va, lsb;
 	const double LSB = 0.00483789062;
 	int NumVa;
 	
+cout << "Introduce lsb: "; cin >> lsb;	
 	do{
 	cout << "Introduce Va: "; cin >> va;
-	NumVa = (va + LSB/2)/LSB;
+	NumVa = (va + lsb/2)/lsb;
 	cout << "Numero Va: " << NumVa;
 	cout << endl;
 	cout << endl;
-    }while(va != 9);
+    }while(true);
 	
 	return 0;
 }
